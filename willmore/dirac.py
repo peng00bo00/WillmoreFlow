41b3afa6fda0883e61dd2cbd3b8c082138bdc95d
peng00bo00 <pengbo_tongji@126.com>
2022-07-19 16:47:30 +0400

    Update dirac.py

--- willmore/dirac.py
+++ willmore/dirac.py
@@ -63,4 +63,42 @@ def buildDirac(mesh: Mesh, rho: np.ndarray) -> QuaternionMatrix:
     for i in range(len(mesh.F)):
         D[i, i]-= rho[i]
     
+    ## R
+    V = mesh.V
+    F = mesh.F
+
+    TT, TTi = igl.triangle_triangle_adjacency(F)
+    edge_mapping = np.array([[0, 1], [1, 2], [2, 0]])
+    left_vertex  = np.array([2, 0, 1])
+
+    R = QuaternionMatrix((len(F), len(F)))
+
+    ## weights c
+    edges = igl.edges(F)
+    edges = V[edges[:,1]] - V[edges[:,0]]
+    edges = np.sqrt(np.sum(edges*edges, axis=1))
+    c = 0.001*np.max(edges)
+
+    for Fi in range(len(F)):
+        for j in range(3):
+            Fj = TT[Fi, j]
+
+            ## find common edge of the two adjacent faces
+            vi, vj = F[Fj][edge_mapping[TTi[Fi, j]]]
+            vk = F[Fj][left_vertex[TTi[Fi, j]]]
+
+            u = V[vi] - V[vk]
+            v = V[vj] - V[vk]
+
+            ## (half) dual edge
+            cot = np.dot(u, v) / np.linalg.norm(np.cross(u, v))
+            w = 0.5 * cot * np.linalg.norm(V[vi] - V[vj]) * c
+
+            R[Fi, Fi] += 0.5*w
+            R[Fi, Fj] -= 0.5*w
+            R[Fj, Fi] -= 0.5*w
+            R[Fj, Fj] += 0.5*w
+    
+    D = D + R
+    
     return D
